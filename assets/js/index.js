8bfda4341a7ea4b72b5e4e105b04fabfb4e48d4b
zhangyu <18217782629@163.com>
2022-06-10 17:16:19 +0800

    finsh home

---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+$(function () {
+
+    getUserInfo();
+
+    var layer = layui.layer;
+    $('.btnLogout').on('click', function () {
+        layer.confirm('确定退出登陆?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            localStorage.removeItem('token');
+            location.href = '/login.html'
+            layer.close(index);
+        });
+
+    })
+
+
+
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        success: function (res) {
+
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data);
+        }
+    })
+}
+
+//渲染用户头像
+function renderAvatar(user) {
+    //获取用户名称
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    if (user.user_pic != null) {
+        //渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        //渲染文本头像
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
